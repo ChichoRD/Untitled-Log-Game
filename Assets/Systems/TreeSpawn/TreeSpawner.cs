790512a104a9b6090d762413550982fb8926c71b
imVictorVM <vvalen01@ucm.es>
2023-12-17 21:44:14 +0100

    tree movement

---
+++ Assets/Systems/TreeSpawn/TreeSpawner.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform _spawnTransform;
+
+    [SerializeField]
+    private Transform[] _treeTransforms;
+
+
+
+}
